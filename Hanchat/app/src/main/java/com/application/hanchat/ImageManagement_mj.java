915595673a70f4dfd73bf99e68041cfb8c3c7f5d
김민지 <39117025+mjkim103301@users.noreply.github.com>
2019-09-07 18:42:39 +0900

    이미지 기능 연결_김민지

---
+++ Hanchat/app/src/main/java/com/application/hanchat/ImageManagement_mj.java
@@ -0,0 +1,159 @@
+package com.application.hanchat;
+
+import android.Manifest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+
+import android.database.Cursor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import android.provider.MediaStore;
+import android.net.Uri;
+import android.util.Base64;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+
+public class ImageManagement_mj extends AppCompatActivity {
+
+    Bitmap image; //사용되는 이미지
+    public Uri uri;
+
+    static int PICK_IMAGE_REQUEST = 1;
+
+    static final String TAG = "MainActivity";
+
+    //권한요청
+    public void tedPermission() {
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                // 권한 요청 성공
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                // 권한 요청 실패
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(getResources().getString(R.string.permission_2))
+                .setDeniedMessage(getResources().getString(R.string.permission_1))
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+
+
+    }
+    public void loadImage(){
+        //Intent 생성
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT); //ACTION_PIC과 차이점?
+        intent.setType("image/*"); //이미지만 보이게
+        //Intent 시작 - 갤러리앱을 열어서 원하는 이미지를 선택할 수 있다.
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+        Toast.makeText(this, "원하는 이미지 선택", Toast.LENGTH_LONG).show();
+
+    }
+
+    //이미지 선택작업 후의 결과 처리
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            //이미지를 하나 골랐을때
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
+                //data에서 절대경로로 이미지를 가져옴
+
+                uri = data.getData();
+
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                //이미지가 한계이상(?) 크면 불러 오지 못하므로 사이즈를 줄여 준다.
+
+                int nh = (int) (bitmap.getHeight() * (1024.0 / bitmap.getWidth()));
+                image= Bitmap.createScaledBitmap(bitmap, 1024, nh, true);
+
+                //base64
+                base64(image, Bitmap.CompressFormat.JPEG, 100);
+
+            } else {
+                Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_LONG).show();
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(this, "Oops! 로딩에 오류가 있습니다.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+        String real_path=getRealImagePath(uri);
+        SendImage();
+
+    }
+    //실제파일 경로 가져오기
+    public String getRealImagePath(Uri uri)
+    {
+
+        String[] proj = { MediaStore.Images.Media.DATA };
+
+        Cursor cursor = getContentResolver().query(uri, proj, null, null, null);
+        int index=cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String path=cursor.getString(index);
+      /*  try {
+            base64(path);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }*/
+        return path;
+
+    }
+    String encodedImage;
+    public void base64(Bitmap scaled, Bitmap.CompressFormat compressFormat, int quality)
+    {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+        encodedImage=Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+
+    }
+
+    //이미지 서버처리
+    HTTPConnecter connecter;
+    String des="";
+    //     //Send버튼 클릭시 이미지를 서버로 보냄.
+    public void SendImage(){
+        connecter=new HTTPConnecter("18.219.204.210", 55252);
+        des=encodedImage;
+        try{
+            Map<String, String> data=new HashMap<>();
+
+            data.put("image", des);
+
+
+            connecter.sendImage("/apptest/test", data, null, new HTTPConnecter.Callback() {
+                @Override
+                public Object DataReceived(String ReceiveString) {
+                    return ReceiveString;
+                }
+
+                @Override
+                public void HandlerMethod(Object obj) {
+                    Toast.makeText(getApplicationContext(), (String) obj, Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ Hanchat/app/src/main/java/com/application/hanchat/MainActivity.java
@@ -0,0 +1,47 @@
+package com.application.hanchat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    HTTPConnecter connecter;
+    ImageManagement_mj imageManagement;
+    final String IP = "18.219.204.210";
+
+    EditText et_chat;
+    Button bt_chat;
+    Button bt_image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        connecter = new HTTPConnecter(IP, 55252);
+
+        et_chat = findViewById(R.id.et_chat);
+        bt_chat = findViewById(R.id.bt_chat);
+        bt_image = findViewById(R.id.bt_image);
+
+        ButtonSetting();
+    }
+
+    //버튼 세팅들은 여기에
+    private void ButtonSetting(){
+        bt_chat.setOnClickListener(new ButtonAction(this, connecter, et_chat));
+    }
+    // + 버튼 눌렀을때 실행됨(나 다른방법 써서 버튼 세팅 안할듯)
+    public void loadImagefromGallery(View view) {
+
+        imageManagement=new ImageManagement_mj();
+
+
+    }
+    //이미지파일을 보내려면 /apptest/test로 sendImage
+}
